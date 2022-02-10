2e956d07c9967015f41d6e9af34a733c32708292
nomikhokher <nomankhokher786@gmail.com>
2022-02-10 18:30:41 +0000

    --configured tailwind

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { MobileWrapperComponent } from './mobile-wrapper/mobile-wrapper.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'wrapper', component: MobileWrapperComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
---
+++ src/app/mobile-wrapper/mobile-wrapper.component.spec.ts
@@ -0,0 +1,25 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { MobileWrapperComponent } from './mobile-wrapper.component';
+
+describe('MobileWrapperComponent', () => {
+  let component: MobileWrapperComponent;
+  let fixture: ComponentFixture<MobileWrapperComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ MobileWrapperComponent ]
+    })
+    .compileComponents();
+  });
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(MobileWrapperComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
