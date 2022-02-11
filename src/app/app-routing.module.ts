deddcdf387b48d786d62de03472748c07d1b5a08
nomikhokher <nomankhokher786@gmail.com>
2022-02-11 18:19:37 +0000

    wallet + profile

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { ExampleScreenComponent } from './nft-screens/example-screen/example-screen.component';
+import { WalletComponent } from './nft-screens/wallet/wallet.component';
+import { ProfileComponent } from './nft-screens/profile/profile.component';
 
 const routes: Routes = [
   { path: 'example-screen', component: ExampleScreenComponent },
+  { path: 'wallet', component: WalletComponent },
+  { path: 'profile', component: ProfileComponent },
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,16 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { MobileWrapperComponent } from './mobile-wrapper/mobile-wrapper.component';
 import { ExampleScreenComponent } from './nft-screens/example-screen/example-screen.component';
+import { WalletComponent } from './nft-screens/wallet/wallet.component';
+import { ProfileComponent } from './nft-screens/profile/profile.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     MobileWrapperComponent,
-    ExampleScreenComponent
+    ExampleScreenComponent,
+    WalletComponent,
+    ProfileComponent
   ],
   imports: [
     BrowserModule,
